5a7eea7a5fa7fd4b8fe6dfc5d4a1af608d9ed29c
chameleon05119 <chameleon05119@gmail.com>
2024-01-13 09:07:25 +0900

    コンポーネント側でストアを利用する

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const count = ref(0);
+import { ref, computed } from "vue";
+import { useCounterStore } from "@/stores/counter";
+
+const counterStore = useCounterStore();
+const count = computed((): number => {
+  return counterStore.counter;
+});
+const doubleCount = computed((): number => {
+  return counterStore.doubleCount;
+});
+const onIncrementClick = (): void => {
+  counterStore.incrementCount();
+};
 </script>
 
 <template>
   <p>現在のポイント：{{ count }}</p>
-  <p>現在のポイント更に倍：{{ count }}</p>
-  <button type="button">加算</button>
+  <p>現在のポイント更に倍：{{ doubleCount }}</p>
+  <button type="button" @click="onIncrementClick">加算</button>
 </template>
