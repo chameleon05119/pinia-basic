9dead9f9814c5fbfcaf610d1d37d21aac7ba1823
chameleon05119 <chameleon05119@gmail.com>
2024-01-13 09:02:33 +0900

    カウンター用のコンポーネントを追加

---
+++ src/components/Counter.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <p>現在のポイント：{{ count }}</p>
+  <p>現在のポイント更に倍：{{ count }}</p>
+  <button type="button">加算</button>
+</template>
