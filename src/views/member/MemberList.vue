965c3851b952f1bb4204810ec07e176df3aa2ebc
chameleon05119 <chameleon05119@gmail.com>
2024-01-21 10:13:51 +0900

    サジェストテスト

--- src/views/member/MemberList.vue
+++ src/views/member/MemberList.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import { useMemberStore } from "@/stores/member";
 import type { Member } from "@/interfaces";
 
+type SplitText = {
+  forwardText: string;
+  backwardText: string;
+};
+
 const memberStore = useMemberStore();
 memberStore.prepareMemberList();
+
+const userInputText = ref("");
+const suggestedText = ref("");
 
 const memberList = computed((): Map<number, Member> => {
   return memberStore.memberList;
@@ -13,8 +21,33 @@ const memberList = computed((): Map<number, Member> => {
 const isEmptyList = computed((): boolean => {
   return memberStore.isMemberListEmpty;
 });
+const displaySuggestedText = computed((): SplitText => {
+  return separateText(suggestedText.value);
+});
+
+const fetchSuggestedText = (userInputText: string): string => {
+  const suggestedText = userInputText + "えええ";
+  return suggestedText;
+};
+const separateText = (text: string): SplitText => {
+  if (!text.startsWith(userInputText.value))
+    return { forwardText: "", backwardText: text };
+  const splitText = {
+    forwardText: userInputText.value,
+    backwardText: text.substring(userInputText.value.length),
+  };
+  return splitText;
+};
+
+watch(userInputText, () => {
+  suggestedText.value = fetchSuggestedText(userInputText.value);
+});
 </script>
 <template>
+  <h2>サジェストテスト</h2>
+  <input type="text" v-model="userInputText" />
+  <span v-text="displaySuggestedText.forwardText" class="forward-text" />
+  <span v-text="displaySuggestedText.backwardText" class="backward-text" />
   <h1>会員情報</h1>
   <nav id="breadcrumbs">
     <ul>
@@ -41,3 +74,14 @@ const isEmptyList = computed((): boolean => {
   </section>
   <RouterView />
 </template>
+<style scoped>
+.forward-text {
+  color: white;
+  font-weight: 400;
+}
+
+.backward-text {
+  color: white;
+  font-weight: bold;
+}
+</style>
